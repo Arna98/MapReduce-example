b687f7dca1c23d8bb4a52c0f17cea85ecca54f0a
Alireza Naseri <alireza.real98@gmail.com>
2022-11-24 01:48:38 +0330

    Implementation of the reducer method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        mapper();
+        String[] str = mapper();
+        reducer(str);
     }
     static String[] mapper(){
         String[] word;
@@ -20,4 +25,31 @@ public class Main {
         return word;
     }
 
+    static void reducer(String[] input){
+        String[] str = input;
+        str = new HashSet<String>(Arrays.asList(str)).toArray(new String[0]);
+        int count = 0;
+        while(count < str.length){
+            int counter = 0;
+            for(String currentWord : input){
+                if(str[count].equals(currentWord)){
+                    counter++;
+                }
+            }
+            System.out.printf("<%s, %d>\n", str[count], counter);
+            try {
+                FileWriter output = new FileWriter("output.txt",true);
+                output.write(String.format("<%s, %d>\n", str[count], counter));
+
+                output.close();
+                System.out.println("Successfully wrote to the file.");
+
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            count++;
+        }
+
+    }
 }
