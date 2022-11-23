eefb46b4b8f2818485e308018b60b39f8d5a662e
Alireza Naseri <alireza.real98@gmail.com>
2022-11-24 01:47:17 +0330

    Implementation of the mapper method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        mapper();
     }
+    static String[] mapper(){
+        String[] word;
+        String tokenize = "\\s+";
+        System.out.print("Enter Input: ");
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine().strip().toLowerCase();
+
+        word = str.split(tokenize);
+
+        for ( String key : word) {
+            System.out.printf("<%s  ,%s>\n", key, "1");
+        }
+        System.out.println("\n------------------------------------------------\n");
+        return word;
+    }
+
 }
