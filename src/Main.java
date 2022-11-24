17cf32ad2cdf0762b727102b2c259976248ed6ee
Alireza Naseri <alireza.real98@gmail.com>
2022-11-24 12:46:53 +0330

    define variables for reducer

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -11,7 +12,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         mapper();
-        //reducer();
+        reducer();
     }
     static void mapper(){
         // Definition of variables
@@ -45,31 +46,11 @@ public class Main {
         }
     }
 
-    static void reducer(String[] input){
-        String[] str = input;
-        str = new HashSet<String>(Arrays.asList(str)).toArray(new String[0]);
-        int count = 0;
-        while(count < str.length){
-            int counter = 0;
-            for(String currentWord : input){
-                if(str[count].equals(currentWord)){
-                    counter++;
-                }
-            }
-            System.out.printf("<%s, %d>\n", str[count], counter);
-            try {
-                FileWriter output = new FileWriter("output.txt",true);
-                output.write(String.format("<%s, %d>\n", str[count], counter));
-
-                output.close();
-                System.out.println("Successfully wrote to the file.");
-
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            count++;
-        }
+    static void reducer(){
+        // Definition of variables
+        String line;
+        String[] words;
+        HashMap<String, Integer> keyValue = new HashMap<String, Integer>();
 
     }
 }
